33ff3dfbc0cdb51e63777b7495f2923c90c8a2c2
İlker Doğan <ilkerdogan1215@gmail.com>
2021-05-27 17:38:27 +0300

    Department classes were created.

---
+++ UniversityInformationSystem/Controllers/DepartmentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UniversityInformationSystem.Models;
+
+namespace UniversityInformationSystem.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public ActionResult Index()
+        {
+            var context = new Context();
+            var departmenties = context.Departments.ToList();
+            var model = new List<DepartmentModel>();
+            foreach (var item in departmenties)
+            {
+                var departmentName = item.BolumAd;
+                model.Add(new DepartmentModel { BolumAd = departmentName });
+            }
+            return View(model);
+        }
+        public ActionResult Create()
+        {
+            var context = new Context();
+            return View();
+        }
+    }
+}
