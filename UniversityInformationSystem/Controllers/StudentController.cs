022722b69f12f9643b5be618677d09bcd0aa9356
İlker Doğan <ilkerdogan1215@gmail.com>
2021-05-27 17:39:17 +0300

    Student classes were created.

---
+++ UniversityInformationSystem/Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UniversityInformationSystem.Models;
+
+namespace UniversityInformationSystem.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ActionResult Index()
+        {
+            var context = new Context();
+            var students = context.Students.ToList();
+            var model = new List<StudentModel>();
+            foreach (var item in students)
+            {
+                var ogrenciAd = item.Ad;
+                var ogrenciSoyad = item.Soyad;
+                model.Add(new StudentModel { Ad = ogrenciAd, Soyad = ogrenciSoyad });
+                
+            }
+            return View(model);
+        }
+        public ActionResult Create()
+        {
+            var context = new Context();
+            return View();
+        }
+    }
+}
---
+++ UniversityInformationSystem/Models/StudentModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UniversityInformationSystem.Models
+{
+    public class StudentModel
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+    }
+}
