118d80c2bf323bdf858d030e613779d64237f8c1
İlker Doğan <ilkerdogan1215@gmail.com>
2021-05-29 17:03:16 +0300

    Students were listed on the student query screen using AJAX.

--- UniversityInformationSystem/Controllers/StudentController.cs
+++ UniversityInformationSystem/Controllers/StudentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UniversityInformationSystem.Entities;
 using UniversityInformationSystem.Models;
 
 namespace UniversityInformationSystem.Controllers
@@ -23,10 +24,35 @@ namespace UniversityInformationSystem.Controllers
             }
             return View(model);
         }
+
         public ActionResult Create()
         {
             var context = new Context();
             return View();
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        public ActionResult SearchLessons(int ogrenciNo)
+        {
+            var context = new Context();
+            var student = context.Students.FirstOrDefault(a => a.OgrenciNo == ogrenciNo);
+            var lessons = new List<StudentLesson>();
+            if (student != null)
+            {
+                lessons = context.StudentLessons.Where(a => a.OgrenciNo == student.OgrenciNo).ToList();
+            }
+            var models = new List<LessonModel>();
+            foreach (var item in lessons)
+            {
+                var model = new LessonModel();
+                model.DersAdi = item.Lesson.DersAdi;
+                models.Add(model);
+            }
+            return PartialView("_Lessons", models);
         }
     }
 }
