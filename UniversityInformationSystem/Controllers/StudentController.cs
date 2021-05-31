fc31c1c56ae5acb68dec1bc4a8e0ca8b0364a05f
İlker Doğan <ilkerdogan1215@gmail.com>
2021-05-31 21:39:09 +0300

    The CRUD process has been completed on the student page.

--- UniversityInformationSystem/Controllers/StudentController.cs
+++ UniversityInformationSystem/Controllers/StudentController.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using UniversityInformationSystem.Entities;
 using UniversityInformationSystem.Models;
@@ -19,8 +17,9 @@ namespace UniversityInformationSystem.Controllers
             {
                 var ogrenciAd = item.Ad;
                 var ogrenciSoyad = item.Soyad;
-                model.Add(new StudentModel { Ad = ogrenciAd, Soyad = ogrenciSoyad });
-                
+                var ogrenciNo = item.OgrenciNo;
+                var departmentName = item.Department.BolumAd;
+                model.Add(new StudentModel { Ad = ogrenciAd, Soyad = ogrenciSoyad, Id = ogrenciNo, DepartmentName = departmentName });
             }
             return View(model);
         }
@@ -28,7 +27,68 @@ namespace UniversityInformationSystem.Controllers
         public ActionResult Create()
         {
             var context = new Context();
-            return View();
+            var model = new StudentModel();
+            var departments = context.Departments.ToList();
+            var kategoriler = new List<SelectListItem>();
+            foreach (var item in departments)
+            {
+                kategoriler.Add(new SelectListItem { Text = item.BolumAd, Value = item.BolumID.ToString() });
+            }
+            model.Departments = new SelectList(kategoriler, "Value", "Text");
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(StudentModel model)
+        {
+            var context = new Context();
+            var student = new Student();
+            student.Ad = model.Ad;
+            student.Soyad = model.Soyad;
+            student.OgrenciNo = model.Id;
+            student.BolumID = model.DepartmentId;
+            context.Students.Add(student);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var context = new Context();
+            var student = context.Students.FirstOrDefault(a => a.OgrenciNo == id);
+            var model = new StudentModel();
+            model.Ad = student.Ad;
+            model.Soyad = student.Soyad;
+            model.DepartmentId = student.BolumID;
+            var departments = context.Departments.ToList();
+            var kategoriler = new List<SelectListItem>();
+            foreach (var item in departments)
+            {
+                kategoriler.Add(new SelectListItem { Text = item.BolumAd, Value = item.BolumID.ToString() });
+            }
+            model.Departments = new SelectList(kategoriler, "Value", "Text");
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(StudentModel model)
+        {
+            var context = new Context();
+            var student = context.Students.FirstOrDefault(a => a.OgrenciNo == model.Id);
+            student.Ad = model.Ad;
+            student.Soyad = model.Soyad;
+            student.BolumID = model.DepartmentId;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var context = new Context();
+            var student = context.Students.FirstOrDefault(a => a.OgrenciNo == id);
+            context.Students.Remove(student);
+            context.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         public ActionResult Search()
--- UniversityInformationSystem/Models/StudentModel.cs
+++ UniversityInformationSystem/Models/StudentModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace UniversityInformationSystem.Models
 {
@@ -10,5 +11,11 @@ namespace UniversityInformationSystem.Models
 
         public string Ad { get; set; }
         public string Soyad { get; set; }
+
+        public int Id { get; set; }
+
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public SelectList Departments { get; set; }
     }
 }
